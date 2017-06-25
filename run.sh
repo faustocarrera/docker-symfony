412101f13cf90f5456f1dfd1c29c8300cece2b4b
Fausto Carrera <fausto.carrera.f@gmail.com>
2017-06-25 16:44:05 -0300

    add run as daemon

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker-compose up --build
+docker-compose up -d --build
 docker exec symfony_php php /app/app/console cache:clear --env=dev
 docker exec symfony_php php /app/app/console cache:clear --env=prod
 docker exec symfony_php chmod -R 777 /app/app/cache
